9796312472c42db3b016dfc240e56bd9b541f6b6
arro38 <etienne.vaytilingom.pro@gmail.com>
2023-11-07 14:37:17 +0400

    Routing ok

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import HomePage from "./pages/HomePage.jsx";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LikePage from "./pages/LikePage.jsx";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/like",
+    element: <LikePage />,
+  },
+  {
+    path: "/*",
+    element: <HomePage />,
+  },
+]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HomePage />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
